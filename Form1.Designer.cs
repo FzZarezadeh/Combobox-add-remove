72ca2bb35da93958f33be5bc00bbd4b7797586c2
FzZarezadeh <fatmhzhrazarzadh@gmail.com>
2025-11-20 17:03:28 +0330

    Combobax add remove

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace prog5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.lbldate = new System.Windows.Forms.Label();
+            this.btnremove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(319, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 126);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(326, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(12, 183);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(160, 26);
+            this.btnadd.TabIndex = 2;
+            this.btnadd.Text = "add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Location = new System.Drawing.Point(12, 57);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(46, 17);
+            this.lbltime.TabIndex = 3;
+            this.lbltime.Text = "label1";
+            // 
+            // lbldate
+            // 
+            this.lbldate.AutoSize = true;
+            this.lbldate.Location = new System.Drawing.Point(12, 88);
+            this.lbldate.Name = "lbldate";
+            this.lbldate.Size = new System.Drawing.Size(46, 17);
+            this.lbldate.TabIndex = 4;
+            this.lbldate.Text = "label2";
+            // 
+            // btnremove
+            // 
+            this.btnremove.Location = new System.Drawing.Point(178, 183);
+            this.btnremove.Name = "btnremove";
+            this.btnremove.Size = new System.Drawing.Size(160, 26);
+            this.btnremove.TabIndex = 2;
+            this.btnremove.Text = "remove";
+            this.btnremove.UseVisualStyleBackColor = true;
+            this.btnremove.Click += new System.EventHandler(this.btnremove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 397);
+            this.Controls.Add(this.lbldate);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.btnremove);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Label lbldate;
+        private System.Windows.Forms.Button btnremove;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prog5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Visible = false;
+            lbltime.Text = dateTimePicker1.Value.ToLongTimeString();
+            lbldate.Text = dateTimePicker1.Value.ToLongDateString();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Length > 0)
+            {
+                comboBox1.Items.Add(comboBox1.Text);
+                comboBox1.Text = "\n";
+            }
+            else
+            {
+                MessageBox.Show("Please write text in box", "Empty textbox", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnremove_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+                comboBox1.Text = "\n";
+            }
+            else
+            {
+                MessageBox.Show("Please select one item", "Item not selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
